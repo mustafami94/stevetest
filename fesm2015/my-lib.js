0964c670ecf05287622844b3b3c3765d242ac128
mustafami94 <71411528+mustafami94@users.noreply.github.com>
2021-08-17 15:39:52 +0300

    Add files via upload

---
+++ fesm2015/my-lib.js
@@ -0,0 +1,63 @@
+import { ɵɵdefineInjectable, ɵsetClassMetadata, Injectable, ɵɵdefineComponent, ɵɵelementStart, ɵɵtext, ɵɵelementEnd, Component, ɵɵdefineNgModule, ɵɵdefineInjector, ɵɵsetNgModuleScope, NgModule } from '@angular/core';
+
+class MyLibService {
+    constructor() { }
+}
+MyLibService.ɵfac = function MyLibService_Factory(t) { return new (t || MyLibService)(); };
+MyLibService.ɵprov = ɵɵdefineInjectable({ token: MyLibService, factory: MyLibService.ɵfac, providedIn: 'root' });
+(function () { (typeof ngDevMode === "undefined" || ngDevMode) && ɵsetClassMetadata(MyLibService, [{
+        type: Injectable,
+        args: [{
+                providedIn: 'root'
+            }]
+    }], function () { return []; }, null); })();
+
+class MyLibComponent {
+    constructor() { }
+    ngOnInit() {
+    }
+}
+MyLibComponent.ɵfac = function MyLibComponent_Factory(t) { return new (t || MyLibComponent)(); };
+MyLibComponent.ɵcmp = ɵɵdefineComponent({ type: MyLibComponent, selectors: [["lib-my-lib"]], decls: 2, vars: 0, template: function MyLibComponent_Template(rf, ctx) { if (rf & 1) {
+        ɵɵelementStart(0, "p");
+        ɵɵtext(1, " my-lib works! ");
+        ɵɵelementEnd();
+    } }, encapsulation: 2 });
+(function () { (typeof ngDevMode === "undefined" || ngDevMode) && ɵsetClassMetadata(MyLibComponent, [{
+        type: Component,
+        args: [{
+                selector: 'lib-my-lib',
+                template: `
+    <p>
+      my-lib works!
+    </p>
+  `,
+                styles: []
+            }]
+    }], function () { return []; }, null); })();
+
+class MyLibModule {
+}
+MyLibModule.ɵfac = function MyLibModule_Factory(t) { return new (t || MyLibModule)(); };
+MyLibModule.ɵmod = ɵɵdefineNgModule({ type: MyLibModule });
+MyLibModule.ɵinj = ɵɵdefineInjector({ imports: [[]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && ɵɵsetNgModuleScope(MyLibModule, { declarations: [MyLibComponent], exports: [MyLibComponent] }); })();
+(function () { (typeof ngDevMode === "undefined" || ngDevMode) && ɵsetClassMetadata(MyLibModule, [{
+        type: NgModule,
+        args: [{
+                declarations: [MyLibComponent],
+                imports: [],
+                exports: [MyLibComponent]
+            }]
+    }], null, null); })();
+
+/*
+ * Public API Surface of my-lib
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { MyLibComponent, MyLibModule, MyLibService };
+//# sourceMappingURL=my-lib.js.map
---
+++ lib/my-lib.module.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./my-lib.component";
+export declare class MyLibModule {
+    static ɵfac: i0.ɵɵFactoryDef<MyLibModule, never>;
+    static ɵmod: i0.ɵɵNgModuleDefWithMeta<MyLibModule, [typeof i1.MyLibComponent], never, [typeof i1.MyLibComponent]>;
+    static ɵinj: i0.ɵɵInjectorDef<MyLibModule>;
+}
+//# sourceMappingURL=my-lib.module.d.ts.map
